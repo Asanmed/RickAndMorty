61af7a236b9c071ab461e8971e0ee9205bbfe924
Asanmed <antoniosanmed@gmail.com>
2021-04-13 15:51:13 +0200

    first commit

---
+++ src/components/CardSet/CardSet.jsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CharCard from '../CharCard/CharCard';
+import './CardSet.css';
+
+const getRandomIds = () => {
+  let output = [];
+  for (let index = 0; index < 6; index++) {
+    output.push(Math.floor(Math.random() * (671 - 1)) + 1);
+  }
+  return output;
+};
+
+const CardSet = () => {
+  const idList = getRandomIds();
+  const [dataFromApi, setDataFromApi] = useState([]);
+  //call to the api
+
+  useEffect(() => {
+    const callApi = async (idList) => {
+      const result = await axios(
+        `https://rickandmortyapi.com/api/character/${idList}`
+      );
+      setDataFromApi(result.data);
+    };
+    callApi(idList);
+  }, []);
+
+  const renderCharCards = () => {
+    return dataFromApi.map((element) => {
+      return (
+        <CharCard
+          key={element.id}
+          imgURL={element.image}
+          name={element.name}
+          status={element.status}
+          specie={element.species}
+          gender={element.gender}
+          episodes={element.episode.length}
+        />
+      );
+    });
+  };
+
+  return <div className="set-container">{renderCharCards()}</div>;
+};
+
+export default CardSet;
---
+++ src/components/CharCard/CharCard.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './CharCard.css';
+const CharCard = ({ imgURL, name, status, specie, gender, episodes }) => {
+  const [stateFontColor, setStateFontColor] = useState(
+    status === 'Alive' ? '#86D95F' : '#F27272'
+  );
+  return (
+    <div className="card-container">
+      <h4 className="card-status-text" style={{ color: `${stateFontColor}` }}>
+        {status}
+      </h4>
+      <img className="card-img" src={imgURL} alt={`Character ${name}`} />
+      <div className="card-data">
+        <h4>
+          <span>Name:</span> {name}
+        </h4>
+
+        <h4>
+          <span>Specie:</span> {specie}
+        </h4>
+        <h4>
+          <span>Gender:</span> {gender}
+        </h4>
+        <h4>
+          <span>Appears in:</span> {episodes} episodes
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default CharCard;
